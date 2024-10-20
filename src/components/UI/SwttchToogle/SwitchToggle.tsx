90d37a06b6a1e7390ae413e35a626b68465e676b
ValeriyKolesnikov <junberg08@gmail.com>
2024-10-20 12:27:37 +0400

    Сделал кастомный чекбокс

---
+++ src/components/UI/SwttchToogle/SwitchToggle.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './switch_toggle.scss'; // Подключаем стили
+
+interface SwitchToggleProps {
+  initialChecked?: boolean;
+  onChange?: (checked: boolean) => void;
+}
+
+const SwitchToggle: React.FC<SwitchToggleProps> = ({ initialChecked = false, onChange }) => {
+  const [checked, setChecked] = useState(initialChecked);
+
+  const toggleSwitch = () => {
+    setChecked(!checked);
+    if (onChange) {
+      onChange(!checked);
+    }
+  };
+
+  return (
+    <div className="tips-container">
+      <div className={`switch ${checked ? 'checked' : ''}`} onClick={toggleSwitch}>
+        <div className="ellipse" />
+      </div>
+    </div>
+  );
+};
+
+export default SwitchToggle;
