77d440aebe3c27f76230199020f36f61da081030
EgorovIvan <sportsmenq@mail.ru>
2024-10-26 11:22:19 +0400

    fix ts13

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -108,7 +108,7 @@ const Filter: React.FC = () => {
     }
 
     // Ввод данных в поле "Дата от"
-    const handleInputStartDate = (value: Date | null | undefined): void => {
+    const handleInputStartDate = (value: Date | null): void => {
         updateInputPeriod((draft) => {
             draft.startDate = value
         })
@@ -116,7 +116,7 @@ const Filter: React.FC = () => {
     }
 
     // Ввод данных в поле "Дата до"
-    const handleInputEndDate = (value: Date | null | undefined): void => {
+    const handleInputEndDate = (value: Date | null): void => {
         updateInputPeriod((draft) => {
             draft.endDate = value
         })
